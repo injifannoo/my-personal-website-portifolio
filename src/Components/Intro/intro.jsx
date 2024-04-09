f3f69a205ca0df91b3b24181193c7679ce824025
injifannoo <injifanotu@gmail.com>
2024-04-09 03:30:00 +0300

    Introduction of the page is updated

--- src/Components/Intro/intro.jsx
+++ src/Components/Intro/intro.jsx
@@ -7,9 +7,14 @@ const Intro = () => {
         <section id='intro'>
             <div className="introContent">
                 <span className='hello'> Wel come,</span>
-                <span className="introName"> I'm Injifannoo Taammiruu Urgeessaa.</span>
-                <p className="introPara"> Skilled MERN stack developer,Flutter developer <br />
-                    Passionate computer Science Student at AAU.</p>
+                <span className="introName"> I'm Enjifano Tammiru Urgessa.</span>
+                <h2 className="introPara"> Skilled MERN stack developer | Flutter developer <br />
+                    Do you have idea to be changes to product? <br/> </h2>
+                    <h3 className="introContact">Here are my Contacts:</h3>
+                    <p className="introList"><li><a href="injifanotu@gmail.com">injifanotu@gmail.com</a></li>
+                    <li> <a href="+251962408198"> +251962408198 </a></li>
+                    </p>
+        
                 <Link activeClass='active' to='myContacts' spy={true} smooth={true} offset={true} ><button className="btn"> Contact me</button></Link>
             </div>
 
